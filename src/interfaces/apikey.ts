91165c376071dfa263905aea849160225b2f1430
T4toh <tatukis@gmail.com>
2024-07-04 11:43:12 -0300

    das

--- src/interfaces/apikey.ts
+++ src/interfaces/apikey.ts
@@ -1,23 +1,27 @@
-import { ICliente } from "./cliente";
+import { ICliente } from './cliente';
 
 export interface IApikey {
   _id?: string;
   //
   identificacion?: string;
   key?: string;
-
   // Permisos
-  global?: boolean;
-  idClientes?: string[];
+  global?: boolean; // Si es global, no se le asignan clientes
+  idCreador?: string; // Si es global. Es lo que puede ver ese cliente.
+  idClientes?: string[]; // Si no es global, se le asignan clientes
+  modulos?: Modulo[]; // Flotas - Alarmas - etc
 
   // Populate
-  clientes?: ICliente[];
+  cliente?: ICliente; // Este sería el creador
+  clientes?: ICliente[]; // Estos son los elegidos para ver
 }
 
-type OmitirCreate = "_id" | "clientes";
+type OmitirCreate = '_id' | 'clientes';
 
 export interface ICreateApikey extends Omit<Partial<IApikey>, OmitirCreate> {}
 
-type OmitirUpdate = "_id" | "clientes";
+type OmitirUpdate = '_id' | 'clientes';
 
 export interface IUpdateApikey extends Omit<Partial<IApikey>, OmitirUpdate> {}
+
+export type Modulo = 'flotas' | 'alarmas';
