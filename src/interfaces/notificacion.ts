6d011bb6772165c26d3bc010b01096db3952b9d4
franrossi9 <fran.rossi.9999@gmail.com>
2024-08-19 14:36:43 -0300

    idUsuario

--- src/interfaces/notificacion.ts
+++ src/interfaces/notificacion.ts
@@ -1,24 +1,27 @@
 import { ICliente } from "./cliente";
 import { IRecordatorio } from "./recordatorio";
+import { IUsuario } from "./usuario";
 
 export interface INotificacion {
   _id?: string;
   idCliente?: string;
+  idUsuario?: string;
   fechaCreacion?: string;
   archivado: boolean;
   idRecordatorio?: string;
 
   // Populate
   cliente?: ICliente;
+  usuario?: IUsuario;
   recordatorio?: IRecordatorio;
 }
 
-type OmitirCreate = "_id" | "cliente" | "recordatorio";
+type OmitirCreate = "_id" | "cliente" | "recordatorio" | "usuario";
 
 export interface ICreateNotificacion
   extends Omit<Partial<INotificacion>, OmitirCreate> {}
 
-type OmitirUpdate = "_id" | "cliente" | "recordatorio";
+type OmitirUpdate = "_id" | "cliente" | "recordatorio" | "usuario";
 
 export interface IUpdateNotificacion
   extends Omit<Partial<INotificacion>, OmitirUpdate> {}
--- src/interfaces/recordatorio.ts
+++ src/interfaces/recordatorio.ts
@@ -1,12 +1,14 @@
 import { ICliente } from "./cliente";
 import { IVehiculo } from "./vehiculo";
 import { IDocumentacion } from "./documentacion";
+import { IUsuario } from "./usuario";
 
 export type TipoRecordatorio = ["km", "fecha"];
 
 export interface IRecordatorio {
   _id?: string;
   idCliente?: string;
+  idUsuario?: string;
   tipo?: TipoRecordatorio;
   fechaLimite?: string;
   fechaCreacion?: string;
@@ -16,16 +18,27 @@ export interface IRecordatorio {
 
   // Populate
   cliente?: ICliente;
+  usuario?: IUsuario;
   vehiculo?: IVehiculo;
   documentacion?: IDocumentacion;
 }
 
-type OmitirCreate = "_id" | "cliente" | "vehiculo" | "documentacion";
+type OmitirCreate =
+  | "_id"
+  | "cliente"
+  | "vehiculo"
+  | "documentacion"
+  | "usuario";
 
 export interface ICreateRecordatorio
   extends Omit<Partial<IRecordatorio>, OmitirCreate> {}
 
-type OmitirUpdate = "_id" | "cliente" | "vehiculo" | "documentacion";
+type OmitirUpdate =
+  | "_id"
+  | "cliente"
+  | "vehiculo"
+  | "documentacion"
+  | "usuario";
 
 export interface IUpdateRecordatorio
   extends Omit<Partial<IRecordatorio>, OmitirUpdate> {}
