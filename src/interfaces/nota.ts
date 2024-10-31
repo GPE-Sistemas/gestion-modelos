c49bcaf1a6d39bf12448df696e95d8bb3cf33a08
Fernando Cortese <cortesefernando@gmail.com>
2024-10-31 15:28:13 -0300

    Agregar nuevas interfaces y tipos para mejorar la gestión de notas y contactos en INota

--- src/interfaces/nota.ts
+++ src/interfaces/nota.ts
@@ -2,18 +2,30 @@ import { IActivo } from "./activo";
 import { ICliente } from "./cliente";
 import { IDispositivoAlarma } from "./dispositivo-alarma";
 
-// export type TipoEntidad = "Cliente" | "Activo" | "Alarma";
+export type TipoNota = "Contacto" | "Nota";
 
+export interface IInformacionNota {
+  nota?: string;
+}
+
+export interface IInformacionContacto {
+  contacto?: string;
+  telefono?: string;
+  email?: string;
+  palabraSeguridadNormal?: string;
+  palabraSeguridadEmergencia?: string;
+}
+
+export type IInformacion = IInformacionNota | IInformacionContacto;
 export interface INota {
   _id?: string;
   idCliente?: string;
-  // tipoEntidad?: TipoEntidad;
-  // Id de: cliente, activo o alarma
   idAsignado?: string;
-  nota?: string;
   permante?: boolean;
   vigenciaDesde?: string;
   vigenciaHasta?: string;
+  tipo?: TipoNota;
+  informacion?: IInformacion;
   // Populate
   cliente?: ICliente;
   activo?: IActivo;
