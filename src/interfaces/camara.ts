d1acff3e335862350b18121c804fc24c6f28619e
Fernando Cortese <cortesefernando@gmail.com>
2024-12-05 17:26:22 -0300

    Actualizar la interfaz ICanalesCamara para usar un objeto con propiedades y modificar la propiedad canales en ICamara para que sea un array.

--- src/interfaces/camara.ts
+++ src/interfaces/camara.ts
@@ -4,11 +4,10 @@ import { IModeloDispositivo } from "./modelo-dispositivo";
 export type TipoHabilitacion = "Siempre" | "Con Evento";
 
 export interface ICanalesCamara {
-  [numero: string]: {
-    nombre?: string;
-    descripcion?: string;
-    tipoHabilitacion?: TipoHabilitacion;
-  };
+  numero: string;
+  nombre?: string;
+  descripcion?: string;
+  tipoHabilitacion?: TipoHabilitacion;
 }
 
 export interface ICamara {
@@ -16,7 +15,7 @@ export interface ICamara {
   idCliente?: string;
   fechaCreacion?: string;
   identificacion?: string;
-  canales?: ICanalesCamara;
+  canales?: ICanalesCamara[];
   idModeloDispositivo?: string;
   host?: string;
   puerto?: number;
