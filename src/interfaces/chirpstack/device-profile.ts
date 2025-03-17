27190e1e916b9122e4084304e6f8e09608423e79
Isaactrl <isaakomerx@gmail.com>
2025-03-17 11:55:03 -0300

    Agregar 'joi-to-swagger' como dependencia en package.json y actualizar package-lock.json

--- src/interfaces/chirpstack/device-profile.ts
+++ src/interfaces/chirpstack/device-profile.ts
@@ -36,3 +36,42 @@ export const IDeviceProfileChirpstackValidation =
       uplinkInterval: joi.string(),
     }),
   });
+
+export const IDeviceProfileChirpstackSwagger = j2s(
+  IDeviceProfileChirpstackValidation
+).swagger;
+
+export interface IDeviceProfileChirpstack {
+  deviceProfile: {
+    classBTimeout?: number;
+    classCTimeout?: number;
+    factoryPresetFreqs: number[];
+    geolocBufferTTL?: number;
+    geolocMinBufferSize?: number;
+    id?: string;
+    macVersion: string;
+    maxDutyCycle?: number;
+    maxEIRP: number;
+    name: string;
+    networkServerID: string;
+    organizationID: string;
+    payloadCodec?: string;
+    payloadDecoderScript?: string;
+    payloadEncoderScript?: string;
+    pingSlotDR?: number;
+    pingSlotFreq?: number;
+    pingSlotPeriod?: number;
+    regParamsRevision: string;
+    rfRegion?: string;
+    rxDROffset1: number;
+    rxDataRate2: number;
+    rxDelay1: number;
+    rxFreq2: number;
+    supports32BitFCnt?: boolean;
+    supportsClassB?: boolean;
+    supportsClassC?: boolean;
+    supportsJoin?: boolean;
+    tags?: Record<string, string>;
+    uplinkInterval: string;
+  };
+}
