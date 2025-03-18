f19003032bb41863a829501dcca1843edda3d256
Isaactrl <isaakomerx@gmail.com>
2025-03-18 17:24:01 -0300

    Agregar la interfaz 'ILoraServer' para la gestión de servidores LoRa

---
+++ src/interfaces/lora-server.ts
@@ -0,0 +1,10 @@
+export interface ILoraServer {
+  _id?: string;
+  nombre?: string;
+  url?: string;
+  // ChirpStack
+  token?: string;
+  organizationID?: string;
+  serviceProfileID?: string;
+  integrationUrl?: string;
+}
