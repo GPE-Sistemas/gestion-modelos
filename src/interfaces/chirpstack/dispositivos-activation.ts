192f21e2946279a1b8affacabdf4dc0b0388a345
Isaactrl <isaakomerx@gmail.com>
2025-03-27 16:40:33 -0300

    Make device activation and keys properties optional in interfaces

--- src/interfaces/chirpstack/dispositivos-activation.ts
+++ src/interfaces/chirpstack/dispositivos-activation.ts
@@ -1,13 +1,13 @@
 export interface ICreateUpdateDeviceActivation {
-  deviceActivation: {
+  deviceActivation?: {
     description?: string;
     aFCntDown?: number;
     appSKey?: string;
     devAddr?: string;
     fCntUp?: number;
-    fNwkSIntKey: string;
-    nFCntDown: number;
-    nwkSEncKey: string;
-    sNwkSIntKey: string;
+    fNwkSIntKey?: string;
+    nFCntDown?: number;
+    nwkSEncKey?: string;
+    sNwkSIntKey?: string;
   };
 }
--- src/interfaces/chirpstack/dispositivos-keys.ts
+++ src/interfaces/chirpstack/dispositivos-keys.ts
@@ -1,6 +1,6 @@
 export interface ICreateUpdateDeviceKeysChirpstack {
-  deviceKeys: {
-    appKey: string;
-    nwkKey: string; //Ambas Keys son lo mismo, pero cambian el nombre según la versión del dispositivo Lorawan
+  deviceKeys?: {
+    appKey?: string;
+    nwkKey?: string; //Ambas Keys son lo mismo, pero cambian el nombre según la versión del dispositivo Lorawan
   };
 }
