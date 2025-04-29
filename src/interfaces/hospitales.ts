d64372f66044dc6bc1aec51793a800ff88f9153d
Isaactrl <isaakomerx@gmail.com>
2025-04-29 11:02:02 -0300

    Refactor hospital and health personnel interfaces to include create and update types

--- src/interfaces/hospitales.ts
+++ src/interfaces/hospitales.ts
@@ -9,3 +9,13 @@ export interface IHospital {
   tipo?: "publico" | "privado" | "publico-privado"; // Tipo de gestión
   activo?: boolean; // Si está operativo
 }
+
+type OmitirCreate = "_id";
+
+export interface ICreateHospital
+  extends Omit<Partial<IHospital>, OmitirCreate> {}
+
+type OmitirUpdate = "_id";
+
+export interface IUpdateHospital
+  extends Omit<Partial<IHospital>, OmitirUpdate> {}
