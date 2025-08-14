176aaa05b49cb7a613c7b5c7b13a64e5737731d5
Isaactrl <isaakomerx@gmail.com>
2025-08-14 09:50:49 -0300

    token push no necesita estar en usuario

--- src/interfaces/usuario.ts
+++ src/interfaces/usuario.ts
@@ -42,7 +42,6 @@ export interface IConfigUsuario {
   cantMapasVehiculos?: number;
   noMostrarGuardarClaveAlarma?: boolean;
   clavesAlarma?: IClaveUsuarioAlarma[];
-  tokenPush?: string; //tokens para las notificaciones push
 }
 
 export interface IClaveUsuarioAlarma {
