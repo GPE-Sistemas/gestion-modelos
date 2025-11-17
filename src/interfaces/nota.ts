8d5a44601032aa5a0d2f62957b75c6e32ddb0075
Isaactrl <isaakomerx@gmail.com>
2025-11-17 13:40:49 -0300

    Refactor nota.ts for consistency in string quotes and add 'interno' field to IInformacionContacto

--- src/interfaces/nota.ts
+++ src/interfaces/nota.ts
@@ -1,9 +1,9 @@
-import { IActivo } from "./activo";
-import { ICliente } from "./cliente";
-import { IDispositivoAlarma } from "./dispositivo-alarma";
-import { ILuminaria } from "./luminaria";
+import { IActivo } from './activo';
+import { ICliente } from './cliente';
+import { IDispositivoAlarma } from './dispositivo-alarma';
+import { ILuminaria } from './luminaria';
 
-export type TipoNota = "Contacto" | "Nota";
+export type TipoNota = 'Contacto' | 'Nota';
 
 export interface IInformacionNota {
   nota?: string;
@@ -12,6 +12,7 @@ export interface IInformacionNota {
 export interface IInformacionContacto {
   contacto?: string;
   telefono?: string;
+  interno?: string;
   email?: string;
 
   // Solo cuando es contacto de alarma
@@ -40,16 +41,16 @@ export interface INota {
   luminaria?: ILuminaria;
 }
 
-type OmitirCreate = "_id" | "cliente";
+type OmitirCreate = '_id' | 'cliente';
 
 export interface ICreateNota extends Omit<Partial<INota>, OmitirCreate> {}
 
-type OmitirUpdate = "_id" | "cliente";
+type OmitirUpdate = '_id' | 'cliente';
 
 export interface IUpdateNota extends Omit<Partial<INota>, OmitirUpdate> {}
 
 export interface INotaCache
   extends Omit<
     INota,
-    "cliente" | "ancestros" | "activo" | "alarma" | "luminaria"
+    'cliente' | 'ancestros' | 'activo' | 'alarma' | 'luminaria'
   > {}
