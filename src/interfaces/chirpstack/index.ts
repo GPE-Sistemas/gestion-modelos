e6ca536fd8aa4f517bb49c68da775b26588bfbd3
Isaactrl <isaakomerx@gmail.com>
2025-12-09 14:36:22 -0300

    Agregar la interfaz IMetricasGateways y sus dependencias en metricas-gateways.ts

--- src/interfaces/chirpstack/index.ts
+++ src/interfaces/chirpstack/index.ts
@@ -3,3 +3,4 @@ export * from './dispositivo-chirpstack';
 export * from './dispositivos-keys';
 export * from './dispositivos-activation';
 export * from './gateway-chirpstack';
+export * from './metricas-gateways';
---
+++ src/interfaces/chirpstack/metricas-gateways.ts
@@ -0,0 +1,21 @@
+export interface IMetricasGateways {
+  rxPackets: IMetricaItem;
+  rxPacketsPerDr: IMetricaItem;
+  rxPacketsPerFreq: IMetricaItem;
+  txPackets: IMetricaItem;
+  txPacketsPerDr: IMetricaItem;
+  txPacketsPerFreq: IMetricaItem;
+  txPacketsPerStatus: IMetricaItem;
+}
+
+export interface IMetricaItem {
+  datasets: IMetricaDataset[];
+  kind: string;
+  name: string;
+  timestamps: string[];
+}
+
+export interface IMetricaDataset {
+  data: number[];
+  label: string;
+}
