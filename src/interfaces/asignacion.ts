1ff2b5e297e8c4b43ca1c0e8dc43af99380a54e1
Waranicaton <carlumoli_00@hotmail.com>
2026-01-16 11:19:59 -0300

    feat(asignacion): add IAsignacion interface and related types for entity assignment

---
+++ src/interfaces/asignacion.ts
@@ -0,0 +1,73 @@
+import { ICliente } from './cliente';
+import { IActivo } from './activo';
+import { ITracker } from './tracker';
+import { IUsuario } from './usuario';
+
+export type IEntidades = 'Chofer' | 'Activo' | 'Tracker';
+
+export interface IAsignacion {
+  _id?: string;
+  //
+  fechaAsignacion?: Date;
+  fechaDesasignacion?: Date;
+  idCliente?: string;
+  idUsuario?: string;
+
+  // Id de la entidad a la que se le asigna algo
+  idEntidadModificada?: string;
+  tipoEntidadModificada?: IEntidades;
+  nombreEntidadModificada?: string;
+  // Id de la entidad que se asigna a la otra
+  idEntidadAsignada?: string;
+  tipoEntidadAsignada?: IEntidades;
+  nombreEntidadAsignada?: string;
+
+  // Populate
+  cliente?: ICliente;
+  usuario?: IUsuario;
+  ChoferModificado?: IUsuario;
+  ActivoModificado?: IActivo;
+  TrackerModificado?: ITracker;
+  ChoferAsignado?: IUsuario;
+  ActivoAsignado?: IActivo;
+  TrackerAsignado?: ITracker;
+}
+
+type OmitirCreate =
+  | '_id'
+  | 'cliente'
+  | 'usuario'
+  | 'ChoferModificado'
+  | 'ActivoModificado'
+  | 'TrackerModificado'
+  | 'ChoferAsignado'
+  | 'ActivoAsignado'
+  | 'TrackerAsignado';
+
+export interface ICreateApikey
+  extends Omit<Partial<IAsignacion>, OmitirCreate> {}
+
+type OmitirUpdate =
+  | '_id'
+  | 'cliente'
+  | 'usuario'
+  | 'ChoferModificado'
+  | 'ActivoModificado'
+  | 'TrackerModificado'
+  | 'ChoferAsignado'
+  | 'ActivoAsignado'
+  | 'TrackerAsignado';
+
+export interface IUpdateApikey
+  extends Omit<Partial<IAsignacion>, OmitirUpdate> {}
+
+export type Modulo =
+  | '_id'
+  | 'cliente'
+  | 'usuario'
+  | 'ChoferModificado'
+  | 'ActivoModificado'
+  | 'TrackerModificado'
+  | 'ChoferAsignado'
+  | 'ActivoAsignado'
+  | 'TrackerAsignado';
