27a3b89997efaf133596560065124e03c05c285d
franrossi9999 <fran.rossi.9999@gmail.com>
2026-01-22 15:55:21 -0300

    feat(log): add ILog and related types for logging functionality

---
+++ src/interfaces/log.ts
@@ -0,0 +1,43 @@
+import { ICliente } from './cliente';
+
+export type TipoEntidadLog =
+  | 'Luminaria'
+  | 'Colectivo'
+  | 'Activo'
+  | 'Tracker'
+  | 'Vehiculo';
+/* ────────────────────────────────────────────────
+ *  REPORTES POR TIPO
+ * ────────────────────────────────────────────────*/
+
+export interface ILogMensaje {
+  mensaje?: string;
+  protocolo?: 'UDP' | 'TCP';
+  origen?: string;
+  puerto?: number;
+}
+
+export type MapaValoresLog = {
+  'Log Mensaje': ILogMensaje;
+};
+
+export interface ILog<T extends keyof MapaValoresLog> {
+  _id: string;
+  fechaCreacion?: string;
+  idCliente?: string;
+  //
+  idsAncestros?: string[];
+  tipoEntidad?: TipoEntidadLog;
+  tipoReporte?: T;
+  valores?: MapaValoresLog[T];
+  // Populate
+  cliente?: ICliente;
+  ancestros?: ICliente[];
+}
+
+////// CREATE
+type Omitir = '_id' | 'idsAncestros' | 'cliente' | 'ancestros';
+export type ICreateLog = Omit<ILog<'Log Mensaje'>, Omitir>;
+
+////// UPDATE
+export type IUpdateLog = Omit<Partial<ILog<'Log Mensaje'>>, Omitir>;
