9e994b0768a70cd90a0f77e03a0bd93ecbde4f78
franrossi9999 <fran.rossi.9999@gmail.com>
2026-01-29 17:33:27 -0300

    fix(hikvision): update EstadoArmadoHikvision type to match API specifications

--- src/interfaces/dispositivo-alarma.ts
+++ src/interfaces/dispositivo-alarma.ts
@@ -619,7 +619,7 @@ export interface ResponseAccessoryInfos {
 
 /// hikvision ( esta hecho para que coincida con la api de mierda que tienen )
 
-export type EstadoArmadoHikvision = 'disarm' | 'arm' | 'stay';
+export type EstadoArmadoHikvision = 'disarm' | 'away' | 'stay';
 
 export interface ISubSysHikvision {
   id?: number;
