07652337a748facad6fcd5d852e5208dd0f88c6a
Tatoh <tatukis@gmail.com>
2026-02-09 12:18:00 -0300

    fix: add missing chipId property to ISirena interface

--- src/interfaces/sirena.ts
+++ src/interfaces/sirena.ts
@@ -9,6 +9,7 @@ export interface ISirena {
   idsAncestros?: string[];
   fechaCreacion?: string;
   idExterno?: string; /// El _id que tiene la sirena en seguridad
+  chipId?: string; /// El chipId de la sirena en seguridad
   idsAsignados?: string[]; // Camaras, sensores, etc
 
   /// Cosas rancias de IRIX
