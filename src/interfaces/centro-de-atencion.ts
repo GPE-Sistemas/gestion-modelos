9cde4d3ac6c362deb1d350d957c3b15dd749a0e2
Isaactrl <isaakomerx@gmail.com>
2026-02-23 15:53:44 -0300

    refactor: comment out unused imports and interfaces in centro-de-atencion, hospitales, and ubicacion files

--- src/interfaces/centro-de-atencion.ts
+++ src/interfaces/centro-de-atencion.ts
@@ -1,44 +1,44 @@
-import { ICliente } from './cliente';
-import { IConfigEventoUsuario } from './config-evento-usuario';
-import { TipoEmergencia } from './emergencias';
-import { IUbicacion } from './ubicacion';
+// import { ICliente } from './cliente';
+// import { IConfigEventoUsuario } from './config-evento-usuario';
+// import { TipoEmergencia } from './emergencias';
+// import { IUbicacion } from './ubicacion';
 
-export interface ICentroDeAtencion {
-  _id?: string;
-  idCliente?: string;
-  idsAncestros?: string[];
+// export interface ICentroDeAtencion {
+//   _id?: string;
+//   idCliente?: string;
+//   idsAncestros?: string[];
 
-  fechaCreacion?: string;
-  nombre?: string;
-  tipoEmergencia?: TipoEmergencia;
-  telefono?: string; // Teléfono de contacto
-  email?: string; // Email institucional
-  activo?: boolean; // Si está operativo
-  idUbicacion?: string;
-  idsConfigsEventosUsuario?: string[];
+//   fechaCreacion?: string;
+//   nombre?: string;
+//   tipoEmergencia?: TipoEmergencia;
+//   telefono?: string; // Teléfono de contacto
+//   email?: string; // Email institucional
+//   activo?: boolean; // Si está operativo
+//   idUbicacion?: string;
+//   idsConfigsEventosUsuario?: string[];
 
-  circuloArea?: {
-    center?: number[];
-    radius?: number;
-  }; //círculo del centro de atención
+//   circuloArea?: {
+//     center?: number[];
+//     radius?: number;
+//   }; //círculo del centro de atención
 
-  //Populate
-  cliente?: ICliente;
-  ancestros?: ICliente[];
-  ubicacion?: IUbicacion;
-  configsEventosUsuario?: IConfigEventoUsuario[];
-}
+//   //Populate
+//   cliente?: ICliente;
+//   ancestros?: ICliente[];
+//   ubicacion?: IUbicacion;
+//   configsEventosUsuario?: IConfigEventoUsuario[];
+// }
 
-type OmitirCreate = '_id';
+// type OmitirCreate = '_id';
 
-export interface ICreateCentroDeAtencion extends Omit<
-  Partial<ICentroDeAtencion>,
-  OmitirCreate
-> {}
+// export interface ICreateCentroDeAtencion extends Omit<
+//   Partial<ICentroDeAtencion>,
+//   OmitirCreate
+// > {}
 
-type OmitirUpdate = '_id';
+// type OmitirUpdate = '_id';
 
-export interface IUpdateCentroDeAtencion extends Omit<
-  Partial<ICentroDeAtencion>,
-  OmitirUpdate
-> {}
+// export interface IUpdateCentroDeAtencion extends Omit<
+//   Partial<ICentroDeAtencion>,
+//   OmitirUpdate
+// > {}
