6967117a19ff28a022bde117036a48632b2ac7e0
Fernando Cortese <cortesefernando@gmail.com>
2026-03-06 12:12:34 -0300

    fix: reorganizar propiedades en la interfaz ISirena para mayor claridad

--- src/interfaces/sirena.ts
+++ src/interfaces/sirena.ts
@@ -1,19 +1,31 @@
+import { IGeoJSONPoint } from '../auxiliares';
 import { ICamara } from './camara';
 import { ICliente } from './cliente';
 import { IModeloDispositivo } from './modelo-dispositivo';
 
+export type EstadoSirena = 'Online' | 'Offline';
+
 export interface ISirena {
   _id?: string;
-  //
+  /// Cosas de IRIX
   idCliente?: string;
   idsAncestros?: string[];
   fechaCreacion?: string;
-  idExterno?: string; /// El _id que tiene la sirena en seguridad
-  chipId?: string; /// El chipId de la sirena en seguridad
+  idModelo?: string;
   idsAsignados?: string[]; // Camaras, sensores, etc
 
-  /// Cosas rancias de IRIX
-  idModelo?: string;
+  // Datos de sincronizacion de seguridad
+  idExterno?: string; /// El _id que tiene la sirena en seguridad
+  chipId?: string; /// El chipId de la sirena en seguridad
+  fechaSincronizacion?: string;
+  ubicacion?: IGeoJSONPoint;
+  direccion?: string;
+  activa?: boolean;
+  estado?: EstadoSirena;
+  onlineDesde?: string;
+  offlineDesde?: string;
+  iccidSim?: string;
+  telefono?: string;
 
   /// Populates
   cliente?: ICliente;
